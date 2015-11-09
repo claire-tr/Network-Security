647689a363388ef08bfcfbac3594853be88632db
cyy427 <cyy427@users.noreply.github.com>
2015-11-09 18:47:09 -0500

    Create PortScan.py
    In this program, assignment, I built a port scanner. This port scanner, PortScan, probes all 2^16 TCP ports on a targeted host, and report the ports that accept connections. The scanner does not require superuser (root) privileges, and can attempt to establish full TCP connections to the tested ports.

---
+++ PortScan.py
@@ -0,0 +1,30 @@
+import sys,socket,datetime,random,time
+count=0
+
+if len(sys.argv)!=2:
+        print "Invalid Input"
+        sys.exit(0)
+else:
+	hostname = sys.argv[1]
+#print hostname
+randomport = [n for n in range(0,65536)]
+random.shuffle(randomport); #get random sequence of the ports
+starttime = datetime.datetime.now()
+for port in randomport:
+#	time.sleep(0.05)
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((hostname,port))
+		print 'Port %s is open, service is %s ' % (port, socket.getservbyport(port))
+		count+=1
+		s.close()
+	except socket.error,msg:
+#		print 'pass'
+		s.close()
+		pass
+endtime = datetime.datetime.now()
+runtime = (endtime - starttime).seconds
+
+print 'run time : %d seconds' % (runtime)
+print 'number of ports found to be open: %d '% (count)
+print 'scan rate : %d (ports scanned per second)' % (65536/runtime)
